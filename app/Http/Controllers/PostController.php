93e51c10ef3643fb54b8ff20e74bda60d61d3016
itora88 <itorak98ftb@gmail.com>
2022-11-02 00:28:17 +0900

    8-4-1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,8 +11,13 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);  
-       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
+    
+    public function create()
+    {
+        return view('posts/create');
     }
 }
 ?>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,22 +2,25 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <title>blog</title>
-
+        <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-       
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
+    <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        <a href='/posts/create'>create</a>
+                    </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
